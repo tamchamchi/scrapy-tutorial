28c1925d238208077dca1af03945f843500d0d54
Tam Anh Nguyen Duy <nguyenduytamanh03012004@gmail.com>
2024-12-28 10:29:30 +0700

    update code for crawl

--- routine/routine/spiders/products.py
+++ routine/routine/spiders/products.py
@@ -1,48 +1,39 @@
 import scrapy
-from scrapy.spiders import CrawlSpider, Rule
-from scrapy.linkextractors import LinkExtractor
 from ..items import RoutineItem
 from scrapy.loader import ItemLoader
 
-
-class ProductsSpider(CrawlSpider):
+class ProductsSpider(scrapy.Spider):
     name = "products"
     allowed_domains = ["routine.vn"]
-    start_urls = ["https://routine.vn/categories/ao-nam?page=1"]
-    
-    # Biến toàn cục để đếm số sản phẩm đã xử lý
+    max_page = 10  # Tổng số trang cần duyệt
+    max_products = 50  # Số sản phẩm tối đa cần thu thập
     product_count = 0
-    max_products = 50  # Số lượng sản phẩm tối đa cần thu thập
 
-    rules = (
-        # Rule để thu thập danh sách sản phẩm
-        Rule(LinkExtractor(allow=(r"page=1",))),
-        # Rule để thu thập các liên kết sản phẩm chi tiết
-        Rule(LinkExtractor(allow=(r"products",)), callback="parse_item"),
-    )
+    def start_requests(self):
+        for page in range(1, self.max_page + 1):
+            url = f"https://routine.vn/categories/ao-nam?page={page}"
+            yield scrapy.Request(url, callback=self.parse_page)
+
+    def parse_page(self, response):
+        # Duyệt qua tất cả các sản phẩm trong trang
+        product_links = response.css('a[href*="products"]::attr(href)').getall()
+        for link in product_links:
+            if self.product_count >= self.max_products:
+                self.logger.info("Reached the maximum product limit. Stopping crawler.")
+                return
+            full_url = response.urljoin(link)
+            yield scrapy.Request(full_url, callback=self.parse_item)
 
     def parse_item(self, response):
-        # Kiểm tra số lượng sản phẩm đã thu thập
-        if self.product_count >= self.max_products:
-            self.logger.info("Reached the maximum product limit. Stopping crawler.")
-            return None
-
-        # Xác định div cha chứa thông tin sản phẩm
         parent_div = response.css('div.sc-7454bded-0.hXtIET.md\\:border-\\[1px\\].z-10.md\\:p-4.md\\:border-border')
 
         if not parent_div:
             self.logger.warning("Parent div not found!")
             return None
 
-        # Tạo ItemLoader để load dữ liệu vào RoutineItem
         l = ItemLoader(item=RoutineItem(), selector=parent_div)
-
-        # Trích xuất thông tin sản phẩm từ parent_div
         l.add_css("name", 'h1.text-\\[22px\\].font-bold::text')
         l.add_css("price", 'div.text-text-primary.font-semibold.text-\\[16px\\]::text')
 
-        # Tăng biến đếm sản phẩm
         self.product_count += 1
-
-        # Trả về item đã thu thập
-        return l.load_item()
+        yield l.load_item()
